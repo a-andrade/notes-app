c1a10f0de0c44d684f762efb0d6040960bd3e93e
Andres Andrade <a.andrade09@yahoo.com>
2021-04-01 19:16:02 -0700

    Init commit

---
+++ notes-app-final.js
@@ -0,0 +1,43 @@
+const notes = [{
+    title: 'My next trip',
+    body: 'I would like to go to Spain'
+}, {
+    title: 'Habbits to work on',
+    body: 'Exercise. Eating a bit better.'
+}, {
+    title: 'Office modification',
+    body: 'Get a new seat'
+}]
+
+const filters = {
+    searchText: ''
+}
+
+const renderNotes = (notes, filters) => {
+    const filteredNotes = notes.filter((note) => {
+        return note.title.toLowerCase().includes(filters.searchText.toLowerCase())
+    })
+
+    document.querySelector('#notes').innerHTML = ''
+
+    filteredNotes.forEach((note) => {
+        const noteElement = document.createElement('p')
+        noteElement.textContent = note.title
+        document.querySelector('#notes').appendChild(noteElement)
+    })
+}
+
+renderNotes(notes, filters)
+
+document.querySelector('#create-note').addEventListener('click', (e) => {
+    e.target.textContent = 'The button was clicked'
+})
+
+document.querySelector('#search-text').addEventListener('input', (e) => {
+    filters.searchText = e.target.value
+    renderNotes(notes, filters)
+})
+
+document.querySelector('#filter-by').addEventListener('change', (e) => {
+    
+})
