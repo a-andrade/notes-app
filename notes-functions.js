c8e8aa4378c3d1a559964e75d779aa691dfc85e3
Andres Andrade <a.andrade09@yahoo.com>
2021-04-06 20:57:44 -0700

    Added displays for sorted notes

--- notes-functions.js
+++ notes-functions.js
@@ -47,8 +47,47 @@ const generateNoteDOM = (note) => {
     return noteElement
 }
 
+// sort notes by one of three ways
+const sortNotes = (notes, sortBy) => {
+    if (sortBy === 'byEdited') {
+        return notes.sort((a, b) => {
+            if (a.updatedAt < b.updatedAt) {
+                return 1
+            } else if (a.updatedAt > b.updatedAt) {
+                return -1
+            } else {
+                return 0
+            }
+        })
+    } else if (sortBy === 'byCreated') {
+        return notes.sort((a, b) => {
+            if (a.createdAt < b.createdAt) {
+                return 1
+            } else if (a.createdAt > b.createdAt) {
+                return -1
+            } else {
+                return 0
+            }
+        })
+    } else if (sortBy === 'alphabetical') {
+        return notes.sort((a, b) => {
+            if (a.title.toLowerCase() < b.title.toLowerCase()) {
+                return -1
+            } else if (a.title.toLowerCase() > b.title.toLowerCase()) {
+                return 1
+            } else {
+                return 0
+            }
+        })
+    } else {
+        return notes
+    }
+}
+
 // render application notes
 const renderNotes = (notes, filters) => {
+    notes = sortNotes(notes, filters.sortBy)
+
     const filteredNotes = notes.filter((note) => {
         return note.title.toLowerCase().includes(filters.searchText.toLowerCase())
     })
